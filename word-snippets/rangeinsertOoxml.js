1a09854decae1aee2668265df720365aa86533b4
Michael Mainer <eyemichael@hotmail.com>
2015-09-23 11:49:41 -0700

    removed license text

--- word-snippets/rangeinsertOoxml.js
+++ word-snippets/rangeinsertOoxml.js
@@ -1,5 +1,3 @@
-/*Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See full license at the bottom of this file.*/
-
 // Run a batch operation against the Word object model.
 Word.run(function (context) {
     
@@ -9,7 +7,8 @@ Word.run(function (context) {
     
     // Queue a commmand to insert OOXML in to the beginning of the range.
     range.insertOoxml("<w:p xmlns:w='http://schemas.microsoft.com/office/word/2003/wordml'> " +
-                      "<w:r><w:rPr><w:b/><w:b-cs/><w:color w:val='FF0000'/><w:sz w:val='28'/>" +                           "<w:sz-cs w:val='28'/></w:rPr><w:t>Hello world (this should be bold," +
+                      "<w:r><w:rPr><w:b/><w:b-cs/><w:color w:val='FF0000'/><w:sz w:val='28'/>" +                           
+                      "<w:sz-cs w:val='28'/></w:rPr><w:t>Hello world (this should be bold," +
                       "red, size 14).</w:t></w:r></w:p>", Word.InsertLocation.start);
     
     // Synchronize the document state by executing the queued-up commands, 
@@ -24,30 +23,3 @@ Word.run(function (context) {
         console.log('Debug info: ' + JSON.stringify(error.debugInfo));
     }
 });
-
-/*
-OfficeJS Snippet Explorer, https://github.com/OfficeDev/office-js-snippet-explorer
-
-Copyright (c) Microsoft Corporation
-All rights reserved.
-
-MIT License:
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
